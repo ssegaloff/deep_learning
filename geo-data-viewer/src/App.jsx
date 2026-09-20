3bb93db86febee4efa64c10b05094c23c3b7465d
ssegaloff <ssegaloff18@gmail.com>
2026-09-19 20:13:46 -0400

    add geo-data-viewer: react app for hw 1

---
+++ geo-data-viewer/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Geographic Data Viewer</h1>
+          <p>
+            This is a geographic data viewer that displays Albemarle County, Virginia archaeological site records from DINAA, sourced via Open Context, with coordinates generalized to ~20km grid cells rather than exact locations, plus attributes like Time Period and Site Category.
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
